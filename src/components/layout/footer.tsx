5e858dc015b29e4b3a992079bf499d4349986fcb
mona-chen <monaoffice20@gmail.com>
2024-05-27 16:14:14 +0100

    feat: implement header and footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright Simpu Blog 2024 - All Right Reserved</p>
+    </div>
+  );
+}
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+// import { Button } from "@/components/ui/button";
+
+export default function Header() {
+  return (
+    <div className="flex flex-row justify-between header">
+      <div>Logo</div>
+
+      <div className="flex flex-row gap-2 header__menu">
+        <p>Home</p>
+        <p>Blog</p>
+        <p>About</p>
+        <p>Login</p>
+      </div>
+
+      <button>Login</button>
+    </div>
+  );
+}
